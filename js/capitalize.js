873b5f6e8aa672e12e09c2e6b05510687b2b4bc4
Ivan Diaz <ivanid22@gmail.com>
2020-09-01 13:18:00 -0300

    Add reverseString method and unit tests

--- js/capitalize.js
+++ js/capitalize.js
@@ -2,15 +2,15 @@ const capOne = (sample) => {
   if (!sample) return null;
   sample = sample.toLowerCase();
   const first = sample[0].toUpperCase();
-  sample = sample.slice(1,sample.length);
+  sample = sample.slice(1, sample.length);
   sample = first + sample;
   return sample;
 };
 
 const capitalize = (sample) => {
   const words = sample.split(' ');
-  const wordsArray = words.map( word =>  capOne(word))
-  return wordsArray.join(' ')
-}
+  const wordsArray = words.map(word => capOne(word));
+  return wordsArray.join(' ');
+};
 
-export default capitalize
+export default capitalize;
--- js/capitalize.test.js
+++ js/capitalize.test.js
@@ -1,28 +1,27 @@
-import capitalize from './capitalize'
+import capitalize from './capitalize';
 
 describe('Capitalize', () => {
   test('Capitalizes a lowercase string', () => {
-    expect(capitalize('lowercase')).toEqual('Lowercase')
-  })
+    expect(capitalize('lowercase')).toEqual('Lowercase');
+  });
 
   test('Does nothing if the string is already capitalized', () => {
-    expect(capitalize('Capitalized')).toEqual('Capitalized')
-  })
+    expect(capitalize('Capitalized')).toEqual('Capitalized');
+  });
 
   test('Capitalizes an uppercase string', () => {
-    expect(capitalize('UPPERCASE')).toEqual('Uppercase')
-  })
+    expect(capitalize('UPPERCASE')).toEqual('Uppercase');
+  });
 
   test('Capitalizes every word in a string', () => {
-    expect(capitalize('every word')).toEqual('Every Word')
-  })
+    expect(capitalize('every word')).toEqual('Every Word');
+  });
 
   test('Does not crash if string is empty', () => {
-    expect(capitalize('')).toEqual('')
-  })
+    expect(capitalize('')).toEqual('');
+  });
 
   test('Does not crash if special characters', () => {
-    expect(capitalize('12ba $asd & z')).toEqual('12ba $asd & Z')
-  })
-
-})
+    expect(capitalize('12ba $asd & z')).toEqual('12ba $asd & Z');
+  });
+});
